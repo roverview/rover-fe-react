e8e4c6dc67b4e91e6091e8cc5787cdab8b8979a9
Melanie Cohen <melaniebcohen@gmail.com>
2018-05-16 16:50:37 -0700

    trying deployment again

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 'use strict';
 
 const express = require('express');
-// const favicon = require('serve-favicon');
 const app = express();
 const PORT = process.env.PORT;
 
-// app.use(favicon(`${__dirname}/favicon.ico`));
 app.use(express.static(`${__dirname}/build`));
 
 app.listen(PORT, () => {
