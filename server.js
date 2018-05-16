2ea37f7d7abc07b796c00b4e420a39c5ae734da0
Melanie Cohen <melaniebcohen@gmail.com>
2018-05-15 22:25:18 -0700

    deploying

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 // const favicon = require('serve-favicon');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 // app.use(favicon(`${__dirname}/favicon.ico`));
 app.use(express.static(`${__dirname}/build`));
