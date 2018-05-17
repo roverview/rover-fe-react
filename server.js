d2fed378063648085198759bff20c7b0a24c6df3
Melanie Cohen <melaniebcohen@gmail.com>
2018-05-16 17:18:46 -0700

    trying deployment again... again

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 'use strict';
 
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
-app.use(express.static(`${__dirname}/build`));
-
-app.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
-});
+express()
+  .use(express.static(`${__dirname}/dist`))
+  .listen(PORT, () => console.log(`listening on PORT ${PORT}`));
