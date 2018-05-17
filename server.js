dd5a97c666ff6916139e9485d91d5b88a5a9bc78
Melanie Cohen <melaniebcohen@gmail.com>
2018-05-16 19:43:29 -0700

    more troubleshooting

--- server.js
+++ server.js
@@ -7,8 +7,4 @@ const app = express();
 
 app.use(express.static(`${__dirname}/dist`));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
-});
-
 app.listen(PORT);
